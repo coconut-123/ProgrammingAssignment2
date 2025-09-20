35976703b7a2cb226dfe62fae3cdde4b59b04a81
coconut-123 <lentheo116@gmail.com>
2025-09-20 11:20:08 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,27 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL   # chỗ lưu nghịch đảo
+
+    # hàm set: gán giá trị mới cho ma trận
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL   # reset cache
+    }
+
+    # hàm get: trả về ma trận gốc
+    get <- function() x
+
+    # hàm setinverse: lưu nghịch đảo vào cache
+    setinverse <- function(inverse) inv <<- inverse
+
+    # hàm getinverse: trả về nghịch đảo nếu đã có trong cache
+    getinverse <- function() inv
+
+    # trả về list các hàm
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 
 }
 
@@ -12,4 +33,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+    
+    # nếu nghịch đảo đã có trong cache thì lấy luôn
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+            # nếu chưa có thì tính nghịch đảo
+    data <- x$get()
+    inv <- solve(data, ...)   # solve() = tính nghịch đảo ma trận trong R
+    
+    # lưu lại vào cache
+    x$setinverse(inv)
+    
+    inv
 }
